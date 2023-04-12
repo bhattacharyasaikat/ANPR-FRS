b5aaac39e53e2db1ed36c367e7f4cd7c2be65060
Saikat Bhattacharya <99088080+bhattacharyasaikat@users.noreply.github.com>
2023-04-12 09:01:24 +0530

    Updated ANPR + FRS
    Saving The face images with Numberplate

--- anpr+frs.py
+++ anpr+frs.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 from pytesseract import pytesseract
-
+import pandas as pd
 
 frameWidth = 640    
 franeHeight = 480   
@@ -12,10 +12,11 @@ face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 minArea = 500
 
-cap =cv2.VideoCapture("both.mp4")
+cap =cv2.VideoCapture("footage.mp4")
 
-cap.set(4,franeHeight)
-cap.set(10,150)
+
+# cap.set(4,franeHeight)
+# cap.set(10,150)
 
 while True:
     success , img  = cap.read() #plate
@@ -29,21 +30,27 @@ while True:
         area = w*h
         if area > minArea:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            #cv2.putText(img,"NumberPlate",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
             imgRoi = img[y:y+h,x:x+w]
             
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),3)
+        area1 = w * h
+        if area1 > minArea:
+            cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),3)
+            imgFace = img[y:y+h,x:x+w]
+
 
     cv2.imshow('Frame',img)
-
-    if cv2.waitKey(30) & 0xFF==ord('q'):
-       # cv2.imwrite("C:\\Users\\HP\Desktop\\Python\\Number_Plate_Detection\\photos"+str(count)+".jpg",imgRoi)
-        cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
-        cv2.waitKey(200)
-        break
+    count=0
+    if cv2.waitKey(10) & 0xFF==ord('q'):
+       cv2.imwrite("C:\\Users\\HP\\Desktop\\abc\\faces"+str(count)+".jpg",imgFace)
+       cv2.imwrite("C:\\Users\\HP\\Desktop\\abc\\photos"+str(count)+".jpg",imgRoi)
+       count = count + 1
+       cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
+       cv2.waitKey(200)
+       break
 cap.release()
 cv2.destroyAllWindows()
+
 
    
